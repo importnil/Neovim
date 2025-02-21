6f29210333b0342cd64919c5649d5d028fe2da01
Vadim Ermolin <vadimyer@gmail.com>
2025-02-21 12:33:03 +0000

    plug: fix Mason popup exception

--- ftplugin/mason.lua
+++ ftplugin/mason.lua
@@ -7,7 +7,7 @@ local win_height = math.ceil(height * 0.7)
 vim.api.nvim_win_set_config(0,
     {
         border = 'rounded',
-        relative = 'win',
+        relative = 'editor',
         width = win_width,
         height = win_height,
         row = math.ceil((height - win_height - 6) / 2),
